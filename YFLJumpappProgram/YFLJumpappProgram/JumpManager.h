6124cad9e32f8c69c1c584742f2fa692cfa11a1f
xiaoyang <xiaoyang@maihaoche.com>
2016-10-20 11:17:40 +0800

    初始化一些方法

---
+++ YFLJumpappProgram/YFLJumpappProgram/JumpManager.h
@@ -0,0 +1,13 @@
+//
+//  JumpManager.h
+//  YFLJumpappProgram
+//
+//  Created by 杨丰林 on 16/10/20.
+//  Copyright © 2016年 杨丰林. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JumpManager : NSObject
+
+@end
---
+++ YFLJumpappProgram/YFLJumpappProgram/JumpManager.m
@@ -0,0 +1,19 @@
+//
+//  JumpManager.m
+//  YFLJumpappProgram
+//
+//  Created by 杨丰林 on 16/10/20.
+//  Copyright © 2016年 杨丰林. All rights reserved.
+//
+
+#import "JumpManager.h"
+
+@implementation JumpManager
+-(instancetype)init{
+    self =[super init];
+    if (self) {
+        
+    }
+    return self;
+}
+@end
