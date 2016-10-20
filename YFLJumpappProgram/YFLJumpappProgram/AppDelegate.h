34790e90f7dcf2de0a4bb5ef1d61b36d5b093387
xiaoyang <xiaoyang@maihaoche.com>
2016-10-20 11:11:21 +0800

    Jumpapp 项目初始化

---
+++ YFLJumpappProgram/YFLJumpappProgram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YFLJumpappProgram
+//
+//  Created by 杨丰林 on 16/10/20.
+//  Copyright © 2016年 杨丰林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YFLJumpappProgram/YFLJumpappProgram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YFLJumpappProgram
+//
+//  Created by 杨丰林 on 16/10/20.
+//  Copyright © 2016年 杨丰林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
